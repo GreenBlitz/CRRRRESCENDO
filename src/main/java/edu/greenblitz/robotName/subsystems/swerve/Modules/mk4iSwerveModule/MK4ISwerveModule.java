6dae1b5590aab11ee8f12b4e3104fd2753ba2285
greenblitz4590 <greenblitz4590@gmail.com>
2024-01-08 22:37:33 +0200

    noam - started updating to phoenix 6

--- src/main/java/edu/greenblitz/robotName/subsystems/swerve/Modules/mk4iSwerveModule/MK4ISwerveModule.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/swerve/Modules/mk4iSwerveModule/MK4ISwerveModule.java
@@ -1,30 +1,35 @@
 package edu.greenblitz.robotName.subsystems.swerve.Modules.mk4iSwerveModule;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.sensors.CANCoder;
-import edu.greenblitz.robotName.subsystems.Battery;
+
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.controls.MotionMagicExpoDutyCycle;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.greenblitz.robotName.subsystems.swerve.Chassis.SwerveChassis;
 import edu.greenblitz.robotName.subsystems.swerve.Modules.ISwerveModule;
 import edu.greenblitz.robotName.subsystems.swerve.Modules.SwerveModuleInputsAutoLogged;
 import edu.greenblitz.robotName.subsystems.swerve.SwerveModuleConfigObject;
 import edu.greenblitz.robotName.utils.Conversions;
-import edu.greenblitz.robotName.utils.motors.GBFalcon;
+import edu.greenblitz.robotName.utils.motors.GBTalonFXPro;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
 public class MK4ISwerveModule implements ISwerveModule {
 
-    private final GBFalcon angularMotor;
-    private final GBFalcon linearMotor;
+    private final GBTalonFXPro angularMotor;
+    private final GBTalonFXPro linearMotor;
     private final CANCoder canCoder;
     private final SimpleMotorFeedforward linearFeedForward;
     private final double encoderOffset;
 
-
+    VelocityTorqueCurrentFOC velocityTorqueCurrentFOC = new VelocityTorqueCurrentFOC(0);
+    MotionMagicExpoDutyCycle motionMagicExpoDutyCycle = new MotionMagicExpoDutyCycle(0)
+            .withEnableFOC(false)
+            .withOverrideBrakeDurNeutral(true);
     public MK4ISwerveModule(SwerveChassis.Module module) {
 
         SwerveModuleConfigObject configObject = switch (module) {
@@ -35,13 +40,22 @@ public class MK4ISwerveModule implements ISwerveModule {
             default -> throw new IllegalArgumentException("Invalid swerve module");
         };
 
-        angularMotor = new GBFalcon(configObject.angleMotorID);
-        angularMotor.config(new GBFalcon.FalconConfObject(MK4iSwerveConstants.ANGULAR_FALCON_CONFIG_OBJECT));
+        angularMotor = new GBTalonFXPro(configObject.angleMotorID);
+        angularMotor.getConfigurator().apply(MK4iSwerveConstants.ANGULAR_FALCON_CONFIG_OBJECT);
 
-        linearMotor = new GBFalcon(configObject.linearMotorID);
-        linearMotor.config(new GBFalcon.FalconConfObject(MK4iSwerveConstants.LINEAR_FALCON_CONF_OBJECT).withInverted(configObject.linInverted));
+        linearMotor = new GBTalonFXPro(configObject.linearMotorID);
+        linearMotor.getConfigurator().apply(MK4iSwerveConstants.LINEAR_FALCON_CONFIG_OBJECT);
+        linearMotor.setInverted(configObject.linInverted);
 
         canCoder = new CANCoder(configObject.AbsoluteEncoderID);
+
+        FeedbackConfigs FEEDBACK_CONFIGS = new FeedbackConfigs();
+        FEEDBACK_CONFIGS.FeedbackRemoteSensorID = canCoder.getDeviceID();
+        FEEDBACK_CONFIGS.FeedbackSensorSource = FeedbackSensorSourceValue.SyncCANcoder;
+        FEEDBACK_CONFIGS.RotorToSensorRatio = MK4iSwerveConstants.ANGULAR_GEAR_RATIO;
+
+        angularMotor.getConfigurator().refresh(FEEDBACK_CONFIGS);
+
         this.encoderOffset = configObject.encoderOffset.getRotations();
 
         this.linearFeedForward = new SimpleMotorFeedforward(MK4iSwerveConstants.ks, MK4iSwerveConstants.kv, MK4iSwerveConstants.ka);
@@ -50,67 +64,63 @@ public class MK4ISwerveModule implements ISwerveModule {
 
     @Override
     public void setLinearVelocity(double speed) {
-        linearMotor.set(
-                TalonFXControlMode.Velocity,
-                speed / MK4iSwerveConstants.LINEAR_TICKS_TO_METERS_PER_SECOND,
-                DemandType.ArbitraryFeedForward,
-                linearFeedForward.calculate(speed) / Battery.getInstance().getCurrentVoltage());
-
+        linearMotor.setControl(velocityTorqueCurrentFOC.withVelocity(speed));
     }
 
     @Override
     public void rotateToAngle(Rotation2d angle) {
-        angularMotor.set(ControlMode.Position, Conversions.MK4IConversions.convertRadiansToTicks(angle));
+        angularMotor.setControl(motionMagicExpoDutyCycle.withPosition(angle.getRadians()));
     }
 
     @Override
     public void setLinearVoltage(double voltage) {
-        linearMotor.set(ControlMode.PercentOutput, voltage / Battery.getInstance().getCurrentVoltage());
+        linearMotor.setVoltage(voltage);
     }
 
     @Override
     public void setAngularVoltage(double voltage) {
-        angularMotor.set(ControlMode.PercentOutput, voltage / Battery.getInstance().getCurrentVoltage());
+        angularMotor.setVoltage(voltage);
     }
 
     @Override
     public void setLinearIdleModeBrake(boolean isBrake) {
-        linearMotor.setNeutralMode(isBrake ? NeutralMode.Brake : NeutralMode.Coast);
+        linearMotor.setNeutralMode(isBrake ? NeutralModeValue.Brake : NeutralModeValue.Coast);
     }
 
     @Override
     public void setAngularIdleModeBrake(boolean isBrake) {
-        angularMotor.setNeutralMode(isBrake ? NeutralMode.Brake : NeutralMode.Coast);
+        angularMotor.setNeutralMode(isBrake ? NeutralModeValue.Brake : NeutralModeValue.Coast);
     }
 
     @Override
     public void resetAngle(Rotation2d angle) {
-        angularMotor.setSelectedSensorPosition(Conversions.MK4IConversions.convertRadiansToTicks(angle));
+        angularMotor.setPosition(angle.getRotations());
     }
+
 
     @Override
     public void stop() {
-        linearMotor.set(ControlMode.PercentOutput, 0);
-        angularMotor.set(ControlMode.PercentOutput, 0);
+        linearMotor.stopMotor();
+        angularMotor.stopMotor();
     }
 
     @Override
     public void updateInputs(SwerveModuleInputsAutoLogged inputs) {
-        inputs.linearVelocity = Conversions.MK4IConversions.convertSensorVelocityToMeterPerSecond(linearMotor.getSelectedSensorVelocity());
-        inputs.angularVelocity =  Conversions.MK4IConversions.convertSensorVelocityToRPM(angularMotor.getSelectedSensorVelocity());
+        inputs.linearVelocity = linearMotor.getVelocity().getValue();
+        inputs.angularVelocity = Conversions.MK4IConversions.convertSensorVelocityToRPM(angularMotor.getVelocity().getValue());
 
-        inputs.linearVoltage = linearMotor.getMotorOutputVoltage();
-        inputs.angularVoltage = angularMotor.getMotorOutputVoltage();
+        inputs.linearVoltage = linearMotor.getSupplyVoltage().getValue();
+        inputs.angularVoltage = angularMotor.getSupplyVoltage().getValue();
 
-        inputs.linearCurrent = linearMotor.getSupplyCurrent();
-        inputs.angularCurrent = angularMotor.getStatorCurrent();
+        inputs.linearCurrent = linearMotor.getSupplyCurrent().getValue();
+        inputs.angularCurrent = angularMotor.getStatorCurrent().getValue();
 
-        inputs.linearMetersPassed = Conversions.MK4IConversions.convertTicksToMeters(linearMotor.getSelectedSensorPosition());
-        inputs.angularPositionRadians = Conversions.MK4IConversions.convertTicksToRadians(angularMotor.getSelectedSensorPosition());
+        inputs.linearMetersPassed = Conversions.MK4IConversions.convertTicksToMeters(linearMotor.getPosition().getValue());
+        inputs.angularPositionRadians = Conversions.MK4IConversions.convertTicksToRadians(angularMotor.getPosition().getValue());
 
-        if (Double.isNaN(Units.degreesToRadians(canCoder.getAbsolutePosition()))){
+        if (Double.isNaN(Units.degreesToRadians(canCoder.getAbsolutePosition()))) {
             inputs.absoluteEncoderPosition = 0;
-        }else{
+        } else {
             inputs.absoluteEncoderPosition = Units.degreesToRadians(canCoder.getAbsolutePosition()) - Units.rotationsToRadians(encoderOffset);
         }
         inputs.isAbsoluteEncoderConnected = canCoder.getFirmwareVersion() != -1;
--- src/main/java/edu/greenblitz/robotName/subsystems/swerve/Modules/mk4iSwerveModule/MK4iSwerveConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/swerve/Modules/mk4iSwerveModule/MK4iSwerveConstants.java
@@ -1,17 +1,17 @@
 package edu.greenblitz.robotName.subsystems.swerve.Modules.mk4iSwerveModule;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.greenblitz.robotName.RobotConstants;
 import edu.greenblitz.robotName.subsystems.swerve.SwerveModuleConfigObject;
-import edu.greenblitz.robotName.utils.PIDObject;
-import edu.greenblitz.robotName.utils.motors.GBFalcon;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class MK4iSwerveConstants {
 	
 	public static SwerveModuleConfigObject MK4I_MODULE_FRONT_LEFT = new SwerveModuleConfigObject(1, 0, 1, Rotation2d.fromRotations(0.8486328125),false);
 	public static SwerveModuleConfigObject MK4I_MODULE_FRONT_RIGHT = new SwerveModuleConfigObject(3, 2, 2,Rotation2d.fromRotations(0.2939453125) ,true);
-	public static SwerveModuleConfigObject MK4I_MODULE_BACK_LEFT = new SwerveModuleConfigObject(5, 4, 3, Rotation2d.fromRotations(0.3024),false);
+	public static SwerveModuleConfigObject MK4I_MODULE_BACK_LEFT = new SwerveModuleConfigObject(5, 4, 3, Rotation2d.fromRotations(0.5524),false);
 	public static SwerveModuleConfigObject MK4I_MODULE_BAK_RIGHT = new SwerveModuleConfigObject(7, 6, 4, Rotation2d.fromRotations(0.8718),true);
 	
 	public static final double ANGULAR_GEAR_RATIO = (150.0 / 7);
@@ -27,15 +27,72 @@ public class MK4iSwerveConstants {
 	public static final double LINEAR_TICKS_TO_METERS_PER_SECOND = RobotConstants.General.Motors.FALCON_VELOCITY_UNITS_PER_RPM / LINEAR_GEAR_RATIO * WHEEL_CIRCUMFERENCE / 60;
 	public static final double ANGLE_TICKS_TO_RADIANS = RobotConstants.General.Motors.FALCON_TICKS_PER_RADIAN / ANGULAR_GEAR_RATIO;
 	public static final double MAG_ENCODER_TICKS_TO_FALCON_TICKS = 2 * Math.PI / ANGLE_TICKS_TO_RADIANS;
-	
-	public static final PIDObject ANGULAR_PID = new PIDObject().withKp(0.05).withMaxPower(1.0).withFF(0);
-	public static final GBFalcon.FalconConfObject ANGULAR_FALCON_CONFIG_OBJECT = new GBFalcon.FalconConfObject().withNeutralMode(NeutralMode.Brake).withCurrentLimit(30).withRampRate(RobotConstants.General.RAMP_RATE_VAL).withInverted(true).withPID(ANGULAR_PID);
-	
-	public static final PIDObject LINEAR_PID = new PIDObject().withKp(0.0003).withMaxPower(0.5);
-	public static final GBFalcon.FalconConfObject LINEAR_FALCON_CONF_OBJECT = new GBFalcon.FalconConfObject()
-			.withNeutralMode(NeutralMode.Brake)
-			.withCurrentLimit(40)
-			.withRampRate(RobotConstants.General.RAMP_RATE_VAL)
-			.withPID(LINEAR_PID);
+
+	public static final TalonFXConfiguration ANGULAR_FALCON_CONFIG_OBJECT = new TalonFXConfiguration();
+	static {
+		Slot0Configs PID_CONFIGS = new Slot0Configs();
+		PID_CONFIGS.kP = 0.05;
+		PID_CONFIGS.kI = 0;
+		PID_CONFIGS.kD = 0;
+		PID_CONFIGS.kS = 0;
+		PID_CONFIGS.kV = 0;
+
+		CurrentLimitsConfigs CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs();
+		CURRENT_LIMITS_CONFIGS.StatorCurrentLimitEnable = true;
+		CURRENT_LIMITS_CONFIGS.StatorCurrentLimit = 40;
+		CURRENT_LIMITS_CONFIGS.SupplyCurrentThreshold = 2;
+
+		ClosedLoopRampsConfigs CLOSED_LOOP_RAMPS_CONFIGS = new ClosedLoopRampsConfigs();
+		CLOSED_LOOP_RAMPS_CONFIGS.VoltageClosedLoopRampPeriod = 0.1;
+
+        var MOTOR_OUTPUT_CONFIGS = new MotorOutputConfigs();
+		MOTOR_OUTPUT_CONFIGS.NeutralMode = NeutralModeValue.Brake;
+		MOTOR_OUTPUT_CONFIGS.Inverted = InvertedValue.CounterClockwise_Positive; //true
+
+		MotionMagicConfigs MOTION_MAGIC_CONFIGS = new MotionMagicConfigs();
+		MOTION_MAGIC_CONFIGS.MotionMagicAcceleration = 2;
+		MOTION_MAGIC_CONFIGS.MotionMagicCruiseVelocity = 2;
+		MOTION_MAGIC_CONFIGS.MotionMagicJerk = 2;
+
+		ANGULAR_FALCON_CONFIG_OBJECT.Slot0 = PID_CONFIGS;
+		ANGULAR_FALCON_CONFIG_OBJECT.CurrentLimits = CURRENT_LIMITS_CONFIGS;
+		ANGULAR_FALCON_CONFIG_OBJECT.ClosedLoopRamps = CLOSED_LOOP_RAMPS_CONFIGS;
+		ANGULAR_FALCON_CONFIG_OBJECT.MotorOutput = MOTOR_OUTPUT_CONFIGS;
+		ANGULAR_FALCON_CONFIG_OBJECT.MotionMagic = MOTION_MAGIC_CONFIGS;
+
+	}
+	public static final TalonFXConfiguration LINEAR_FALCON_CONFIG_OBJECT = new TalonFXConfiguration();
+	static {
+		Slot0Configs PID_CONFIGS = new Slot0Configs();
+		PID_CONFIGS.kP = 0.0003;
+		PID_CONFIGS.kI = 0;
+		PID_CONFIGS.kD = 0;
+		PID_CONFIGS.kS = 0.16411;
+		PID_CONFIGS.kV = 2.6824;
+
+		CurrentLimitsConfigs CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs();
+		CURRENT_LIMITS_CONFIGS.StatorCurrentLimitEnable = true;
+		CURRENT_LIMITS_CONFIGS.StatorCurrentLimit = 40;
+		CURRENT_LIMITS_CONFIGS.SupplyCurrentThreshold = 2;
+
+		ClosedLoopRampsConfigs CLOSED_LOOP_RAMPS_CONFIGS = new ClosedLoopRampsConfigs();
+		CLOSED_LOOP_RAMPS_CONFIGS.VoltageClosedLoopRampPeriod = 0.1;
+
+		TorqueCurrentConfigs TORQUE_CURRENT_CONFIGS = new TorqueCurrentConfigs();
+		TORQUE_CURRENT_CONFIGS.PeakForwardTorqueCurrent = 60;
+		TORQUE_CURRENT_CONFIGS.PeakReverseTorqueCurrent = -60;
+
+		MotorOutputConfigs MOTOR_OUTPUT_CONFIGS = new MotorOutputConfigs();
+		MOTOR_OUTPUT_CONFIGS.NeutralMode = NeutralModeValue.Brake;
+
+		LINEAR_FALCON_CONFIG_OBJECT.Slot0 = PID_CONFIGS;
+		LINEAR_FALCON_CONFIG_OBJECT.CurrentLimits = CURRENT_LIMITS_CONFIGS;
+		LINEAR_FALCON_CONFIG_OBJECT.ClosedLoopRamps = CLOSED_LOOP_RAMPS_CONFIGS;
+		LINEAR_FALCON_CONFIG_OBJECT.TorqueCurrent = TORQUE_CURRENT_CONFIGS;
+		LINEAR_FALCON_CONFIG_OBJECT.MotorOutput = MOTOR_OUTPUT_CONFIGS;
+
+
+
+	}
 	public static final double LINEAR_MOTOR_FREE_CURRENT = 1.5;
 }
