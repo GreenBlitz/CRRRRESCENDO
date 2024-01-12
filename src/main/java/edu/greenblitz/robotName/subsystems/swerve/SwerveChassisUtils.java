7fd771511b67a67542946e723508d53fee67796c
NoamRosenberg08 <noamrosenberg097@gmail.com>
2024-01-12 21:37:30 +0200

    noam - cr

--- src/main/java/edu/greenblitz/robotName/subsystems/swerve/SwerveChassisUtils.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/swerve/SwerveChassisUtils.java
@@ -5,12 +5,8 @@ public class SwerveChassisUtils {
 
 
 
-    public static double joystickValueToLinearVelocity(double joystickValue, double velocityFactor) {
-        return joystickValue * velocityFactor * (ChassisConstants.LINEAR_JOYSTICK_INVERTED ? -1 : 1);
-    }
-
-    public static double joystickValueToAngularVelocity(double joystickValue, double velocityFactor) {
+    public static double joystickValueToOutputValue(double joystickValue, double velocityFactor, boolean isInverted) {
         double factoredOutputValue = joystickValue * velocityFactor;
-        return Math.min(factoredOutputValue, velocityFactor) * (ChassisConstants.ANGULAR_JOYSTICK_INVERTED ? -1 : 1);
-    }
+        return Math.min(factoredOutputValue, velocityFactor) * (isInverted ? -1 : 1);    }
+
 }
