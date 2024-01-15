7747d2f085fbda2bc0cea1c91211a039e2fb782f
GreenBlitz <greenblitz4590@gmail.com>
2024-01-15 18:05:30 +0200

    Romy - fixed cr reqursts

--- src/main/java/edu/greenblitz/robotName/commands/swerve/Battery/BatteryLimiter.java
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/Battery/BatteryLimiter.java
@@ -4,45 +4,33 @@ import edu.greenblitz.robotName.subsystems.Battery;
 import edu.greenblitz.robotName.utils.GBCommand;
 import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-import static com.sun.org.apache.bcel.internal.util.Args.require;
 
 public class BatteryLimiter extends GBCommand {
 
-    private static final int LEN_OF_AVG = 50;
-    private static final double USAGE_MIN_LEVEL = 2;
+    private static final int WINDOW_SIZE = 50;
     private Battery battery;
     private LinearFilter voltageFilter;
 
 
     public BatteryLimiter() {
-        voltageFilter = LinearFilter.movingAverage(LEN_OF_AVG);
-        battery = Battery.getInstance();
         require(battery);
+        voltageFilter = LinearFilter.movingAverage(WINDOW_SIZE);
+        battery = Battery.getInstance();
     }
 
     @Override
     public void initialize() {
-        for (int i = 0; i < LEN_OF_AVG; i++) {
+        for (int i = 0; i < WINDOW_SIZE; i++) {
             voltageFilter.calculate(battery.getCurrentVoltage());
         }
     }
 
     @Override
     public void execute() {
-        double currentAverageVoltage;
-
-        if(battery.getTotalCurrent() <= USAGE_MIN_LEVEL) {
-            currentAverageVoltage = voltageFilter.calculate(battery.getCurrentVoltage());
-
-
-            if (currentAverageVoltage <= battery.getMinimumVoltage()
-                    && DriverStation.getMatchType() == DriverStation.MatchType.None) {
-                CommandScheduler.getInstance().cancelAll();
-                CommandScheduler.getInstance().disable();
-                throw new java.lang.RuntimeException("Battery is low");
-            }
+        double currentAverageVoltage = voltageFilter.calculate(battery.getCurrentVoltage());
+        if (currentAverageVoltage <= battery.getMinimumVoltage()
+                && DriverStation.getMatchType() == DriverStation.MatchType.None) {
+            throw new java.lang.RuntimeException("Battery is low");
         }
     }
 }
