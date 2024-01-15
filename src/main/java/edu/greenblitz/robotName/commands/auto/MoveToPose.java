8893b3f34ed3a4d159dc72f9cddf244e2a580715
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-15 19:44:41 +0200

    hillel - roborio should work now

--- src/main/java/edu/greenblitz/robotName/commands/auto/MoveToPose.java
+++ src/main/java/edu/greenblitz/robotName/commands/auto/MoveToPose.java
@@ -11,13 +11,16 @@ import edu.greenblitz.robotName.utils.FMSUtils;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class MoveToPose {
     public static Command getPathCommand(Pose2d endPoint, PathConstraints constraints) {
+        SmartDashboard.putBoolean("working", true);
         return AutoBuilder.pathfindToPose(
                 endPoint,
                 constraints
         );
+
     }
 }
