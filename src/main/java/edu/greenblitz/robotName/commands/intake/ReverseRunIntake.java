d1d9b37363423f1fdbb661d563c3e9adb5e87e32
itamaroryan <itamar.oryan@gmail.com>
2024-01-22 19:00:49 +0200

    Noam & Hillel - fixed CR requests

--- src/main/java/edu/greenblitz/robotName/commands/intake/ReverseRunIntake.java
+++ src/main/java/edu/greenblitz/robotName/commands/intake/ReverseRunIntake.java
@@ -5,7 +5,7 @@ import edu.greenblitz.robotName.subsystems.Intake.IntakeConstants;
 public class ReverseRunIntake extends IntakeCommand{
     @Override
     public void execute(){
-        intake.setPower(-IntakeConstants.POWER_TO_RUN);
+        intake.setPower(IntakeConstants.POWER_TO_EJECT);
     }
 
     @Override
--- src/main/java/edu/greenblitz/robotName/subsystems/Intake/IntakeConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Intake/IntakeConstants.java
@@ -5,13 +5,5 @@ import edu.wpi.first.math.controller.PIDController;
 public class IntakeConstants {
 	public static final int INTAKE_ID = 0;
 	public static final double POWER_TO_RUN = 0;
-	
-	public static class SimulationConstants {
-		public static final int NUMBER_OF_MOTORS = 0;
-		public static final double GEAR_RATIO = 0;
-		public static final double MOMENT_OF_INERTIA = 0;
-		
-	}
-	
-	
+	public static final double POWER_TO_EJECT = -POWER_TO_RUN;
 }
