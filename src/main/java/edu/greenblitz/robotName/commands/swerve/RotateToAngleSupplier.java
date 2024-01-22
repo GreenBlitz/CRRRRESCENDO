8c296deb7cb9e42cfc17a0a3663c75e9ab9597a7
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-22 19:40:18 +0200

    noam - think fixed bug

--- src/main/java/edu/greenblitz/robotName/commands/swerve/RotateToAngleSupplier.java
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/RotateToAngleSupplier.java
@@ -12,40 +12,45 @@ import java.util.function.Supplier;
 public class RotateToAngleSupplier implements DoubleSupplier {
 
 
-    public Supplier<Rotation2d> angleSupplier;
-    public DoubleSupplier feedForwardSupplier;
-    public PIDController pidController;
+	public Supplier<Rotation2d> angleSupplier;
+	public DoubleSupplier feedForwardSupplier;
+	public PIDController pidController;
 
 
-    public RotateToAngleSupplier (Supplier<Rotation2d> targetAngle){
-        this(targetAngle,() -> 0);
-    }
-    public RotateToAngleSupplier (Supplier<Rotation2d> targetAngle,DoubleSupplier feedForwardSupplier){
-        this.angleSupplier = targetAngle;
-        this.feedForwardSupplier = feedForwardSupplier;
-        pidController = new PIDController(0.4,0,0.1);
-        pidController.setTolerance(Units.degreesToRadians(10));
-        pidController.enableContinuousInput(0,2 * Math.PI);
+	public RotateToAngleSupplier(Supplier<Rotation2d> targetAngle) {
+		this(targetAngle, () -> 0);
+	}
 
-    }
-    @Override
-    public double getAsDouble() {
-        pidController.setSetpoint(angleSupplier.get().getRadians());
-        pidController.setTolerance(Units.degreesToRadians(10));
+	public RotateToAngleSupplier(Supplier<Rotation2d> targetAngle, DoubleSupplier feedForwardSupplier) {
+		this.angleSupplier = targetAngle;
+		this.feedForwardSupplier = feedForwardSupplier;
+		pidController = new PIDController(0.4, 0, 0.1);
+		pidController.setTolerance(Units.degreesToRadians(10));
+		pidController.enableContinuousInput(0, 2 * Math.PI);
+
+	}
+
+	@Override
+	public double getAsDouble() {
+		pidController.setSetpoint(angleSupplier.get().getRadians());
+		pidController.setTolerance(Units.degreesToRadians(10));
 
 
-        double pidOutput = pidController.calculate(
-                SwerveChassis.getInstance().getChassisAngle().getRadians()
-        );
+		double pidOutput = pidController.calculate(
+				SwerveChassis.getInstance().getChassisAngle().getRadians()
+		);
 
-        SmartDashboard.putNumber("setpoint", angleSupplier.get().getDegrees());
-        SmartDashboard.putBoolean("is at setpoint" , isAtSetpoint());
-        SmartDashboard.putBoolean("is at setset" , pidController.atSetpoint());
+		SmartDashboard.putNumber("setpoint", angleSupplier.get().getDegrees());
+		SmartDashboard.putBoolean("is at setpoint", isAtSetpoint());
+		SmartDashboard.putBoolean("is at setset", pidController.atSetpoint());
 
-        return isAtSetpoint() || pidController.atSetpoint() ? 0 : pidOutput + feedForwardSupplier.getAsDouble();
-    }
+		return isAtSetpoint() || pidController.atSetpoint() ? 0 : pidOutput + feedForwardSupplier.getAsDouble();
+	}
 
-    public boolean isAtSetpoint(){
-        return Math.abs(Math.IEEEremainder(angleSupplier.get().unaryMinus().getRadians() - SwerveChassis.getInstance().getChassisAngle().getRadians(), 2 * Math.PI)) < Units.degreesToRadians(10);
-    }
+	public boolean isAtSetpoint() {
+		Rotation2d currentAngle = SwerveChassis.getInstance().getChassisAngle();
+		Rotation2d targetAngle = SwerveChassis.getInstance().getChassisAngle();
+		return (currentAngle.getRadians() - targetAngle.getRadians()) % (2 * Math.PI) < Units.degreesToRadians(5) ||
+				(targetAngle.getRadians() - currentAngle.getRadians()) % (2 * Math.PI) < Units.degreesToRadians(5);
+	}
 }
