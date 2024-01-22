5c73909cabd39ec20a0f7d068963b25740a98db1
greenblitz4590 <greenblitz4590@gmail.com>
2024-01-22 16:34:35 +0200

    Yoni & Yoav - what are thosseeeee merge conflicts

--- src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/ReplayWrist.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/ReplayWrist.java
@@ -1,16 +1,8 @@
-<<<<<<<< HEAD:src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/ReplayWrist.java
 package edu.greenblitz.robotName.subsystems.Arm.EndEffector.Wrist;
 
 import com.revrobotics.CANSparkMax;
 
 public class ReplayWrist implements IWrist {
-========
-package edu.greenblitz.robotName.subsystems.Shooter.Pivot;
-
-import com.ctre.phoenix6.signals.NeutralModeValue;
-
-public class ReplayPivot implements IPivot{
->>>>>>>> dee99b51352662ae3c2fab8acda55e4f8433fded:src/main/java/edu/greenblitz/robotName/subsystems/Shooter/Pivot/ReplayPivot.java
 
     @Override
     public void setPower(double power) {
@@ -38,11 +30,7 @@ public class ReplayPivot implements IPivot{
     }
 
     @Override
-<<<<<<<< HEAD:src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/ReplayWrist.java
     public void updateInputs(WristInputsAutoLogged inputs) {
-========
-    public void updateInputs(PivotInputsAutoLogged inputs) {
->>>>>>>> dee99b51352662ae3c2fab8acda55e4f8433fded:src/main/java/edu/greenblitz/robotName/subsystems/Shooter/Pivot/ReplayPivot.java
 
     }
 }
--- src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/WristInputs.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/WristInputs.java
@@ -1,17 +1,9 @@
-<<<<<<<< HEAD:src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/WristInputs.java
 package edu.greenblitz.robotName.subsystems.Arm.EndEffector.Wrist;
-========
-package edu.greenblitz.robotName.subsystems.Shooter.Pivot;
->>>>>>>> dee99b51352662ae3c2fab8acda55e4f8433fded:src/main/java/edu/greenblitz/robotName/subsystems/Shooter/Pivot/PivotInputs.java
 
 import org.littletonrobotics.junction.AutoLog;
 
 @AutoLog
-<<<<<<<< HEAD:src/main/java/edu/greenblitz/robotName/subsystems/Arm/EndEffector/Wrist/WristInputs.java
 public class WristInputs {
-========
-public class PivotInputs {
->>>>>>>> dee99b51352662ae3c2fab8acda55e4f8433fded:src/main/java/edu/greenblitz/robotName/subsystems/Shooter/Pivot/PivotInputs.java
 
     public double appliedOutput;
 
