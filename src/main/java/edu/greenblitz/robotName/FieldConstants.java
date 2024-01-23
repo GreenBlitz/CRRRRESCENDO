39ac4f9fcc7be6807af59537a45fca49960ad60a
greenblitz4590 <greenblitz.4590@gmail.com>
2024-01-23 20:53:41 +0200

    Yoni - added FieldConstants

---
+++ src/main/java/edu/greenblitz/robotName/FieldConstants.java
@@ -0,0 +1,12 @@
+package edu.greenblitz.robotName;
+
+import edu.wpi.first.math.geometry.Translation3d;
+/**
+ * All constants in this file are measured in meters.
+ *
+ * When talking about 3d objects the z direction is up.
+ * */
+public class FieldConstants {
+
+    public static final Translation3d MID_SPEAKER_POSITION = new Translation3d(2.225,0.23,2.045);
+}
