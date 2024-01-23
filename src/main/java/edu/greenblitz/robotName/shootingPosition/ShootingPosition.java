d7ed9e9dac1bda9840d43a4168df530102bbaefe
bossyShrimp68 <romypine007@gmail.com>
2024-01-23 14:35:01 +0200

    Romy - i dream of working more than 5 mins in a row

--- src/main/java/edu/greenblitz/robotName/shootingPosition/ShootingPosition.java
+++ src/main/java/edu/greenblitz/robotName/shootingPosition/ShootingPosition.java
@@ -1,24 +1,20 @@
 package edu.greenblitz.robotName.shootingPosition;
 
+import com.sun.source.tree.ReturnTree;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.trajectory.constraint.EllipticalRegionConstraint;
 
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.security.spec.EllipticCurve;
 
 import static edu.greenblitz.robotName.shootingPosition.ShootingPositionConstants.*;
 
 public class ShootingPosition {
-    private EllipticCurve curve;
 
     public boolean isInRange(Pose2d pose) {
-        curve = new EllipticCurve(
-
-        );
+        return CURVE.contains(pose.getX(), pose.getY());
     }
 
-    public Pose2d getAnglePose(Pose2d pose) {
+    public Pose2d getClosestToCurvePose(Pose2d pose) {
 
     }
 
