af588036970bd1c0ae8f357fbfa7c76288d85682
GilStein1 <151079574+GilStein1@users.noreply.github.com>
2024-01-24 18:36:35 +0200

    Update src/main/java/edu/greenblitz/robotName/subsystems/Lifter/LifterFactory.java
    Co-authored-by: bossyshrimp68 <114258444+bossyshrimp68@users.noreply.github.com>

--- src/main/java/edu/greenblitz/robotName/subsystems/Lifter/LifterFactory.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Lifter/LifterFactory.java
@@ -5,13 +5,9 @@ import edu.greenblitz.robotName.Robot;
 public class LifterFactory {
     public static ILifter create() {
         switch (Robot.RobotType.ROBOT_NAME) {
-            case ROBOT_NAME -> {
-                return new NeoLifter();
-            }
-            case REPLAY -> {
-                return new LifterReplay();
-            }
+            case ROBOT_NAME -> new NeoLifter();
+            case REPLAY -> new LifterReplay();
+            case SIMULATION -> new SimulationLifter();
         }
-        return new SimulationLifter();
     }
 }
