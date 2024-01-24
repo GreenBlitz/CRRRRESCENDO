539a6e8f1de7b9fc54fe67c03bb86d01974b573f
hillelv <hillel.vider@gmail.com>
2024-01-24 14:52:26 +0200

    hillel- now gripper will stop if there is a note in the robot

--- src/main/java/edu/greenblitz/robotName/subsystems/Intake/IntakeConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Intake/IntakeConstants.java
@@ -4,6 +4,8 @@ import com.revrobotics.SparkMaxLimitSwitch;
 
 public class IntakeConstants {
 	public static final int INTAKE_ID = 0;
+	public static final double POWER_TO_RUN = 0;
+	public static final double POWER_TO_REVERSE_RUN = -POWER_TO_RUN;
 	public static final int ENTRANCE_BEAM_BREAKER_CHANNEL = 0;
 	public static final int EXIT_BEAM_BREAKER_CHANNEL = 0;
 
