9e792edd1c790eaa92d2cb14f427e4b0e0916673
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-27 20:17:10 +0200

    b

--- src/main/java/edu/greenblitz/robotName/OI.java
+++ src/main/java/edu/greenblitz/robotName/OI.java
@@ -31,7 +31,7 @@ public class OI {
     }
 
     public static OI getInstance() {
-        if (instance == null){
+        if (instance == null) {
             instance = new OI();
         }
         return instance;
