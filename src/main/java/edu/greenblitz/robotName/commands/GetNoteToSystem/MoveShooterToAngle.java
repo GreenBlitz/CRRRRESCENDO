cb4c8d4fe67e953c947626d25209fbcde70097da
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-27 20:13:42 +0200

    yoav - command better

--- src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterToAngle.java
+++ src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterToAngle.java
@@ -6,29 +6,28 @@ import edu.greenblitz.robotName.subsystems.arm.Elbow;
 import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.ElbowConstants;
 import edu.greenblitz.robotName.subsystems.arm.EndEffector.WristUtils.WristConstants;
 import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
-import edu.greenblitz.robotName.utils.GBCommand;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class MoveShooterToAngle extends ParallelCommandGroup {
+public class MoveShooterToAngle extends ConditionalCommand {
 
-    public MoveShooterToAngle(Rotation2d targetAngle) {
-        super(
-                new ConditionalCommand(new MoveArmBy2Angles(
-                        ElbowConstants.ImportantPlaces.SAFE.ANGLE,
-                        WristConstants.ImportantPlaces.TRANSFER.ANGLE
-                )
-                        ,new GBCommand() {},
-                        () -> Elbow.getInstance().isInDangerZone()
-                ),
-                new SequentialCommandGroup(
-                        new WaitCommand(PivotConstants.NO_COLLISION_DELAY_SECONDS),
-                        new MovePivotToAngle(targetAngle)
-                        )
-        );
-    }
+	public MoveShooterToAngle(Rotation2d targetAngle) {
+		super(
+				new ParallelCommandGroup(
+						new MoveArmBy2Angles(
+								ElbowConstants.ImportantPlaces.SAFE.ANGLE,
+								WristConstants.ImportantPlaces.TRANSFER.ANGLE
+						),
+						new SequentialCommandGroup(
+								new WaitCommand(PivotConstants.NO_COLLISION_DELAY_SECONDS),
+								new MovePivotToAngle(targetAngle)
+						)
+				),
+				new MovePivotToAngle(targetAngle),
+				() -> Elbow.getInstance().isInDangerZone());
+	}
 
 }
