a6f16cd0c4b668247747b82562efec8e1a7933e5
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-27 17:28:30 +0200

    yoav - names

--- src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/GetToTransferNotePlace.java
+++ src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/GetToTransferNotePlace.java
@@ -7,9 +7,9 @@ import edu.greenblitz.robotName.subsystems.arm.EndEffector.WristUtils.WristConst
 import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
-public class SwitchBetweenSystemsPlace extends ParallelCommandGroup {
+public class GetToTransferNotePlace extends ParallelCommandGroup {
 
-    public SwitchBetweenSystemsPlace() {
+    public GetToTransferNotePlace() {
         super(
                 new MoveArmBy2Angles(
                         ElbowConstants.ImportantPlaces.TRANSFER.ANGLE,
