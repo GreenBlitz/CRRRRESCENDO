4d6629815b0b52250214bad84003e96e6247dad0
greenblitz4590 <greenblitz.4590@gmail.com>
2024-01-27 18:35:29 +0200

    Yoni - deleted commands inside subsysetms

--- src/main/java/edu/greenblitz/robotName/subsystems/arm/elbow/ElbowDefaultCommand.java
+++
@@ -1,9 +0,0 @@
-package edu.greenblitz.robotName.commands.arm.elbow;
-
-public class ElbowDefaultCommand extends ElbowCommand{
-
-    @Override
-    public void execute() {
-        elbow.standInPlace();
-    }
-}
