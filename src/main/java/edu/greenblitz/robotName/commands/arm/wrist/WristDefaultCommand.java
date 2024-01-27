b0361d593ebf5861cb0742d3f884a550defa892c
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-27 18:37:00 +0200

    yoav

--- src/main/java/edu/greenblitz/robotName/commands/arm/wrist/WristDefaultCommand.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/wrist/WristDefaultCommand.java
@@ -6,11 +6,6 @@ public class WristDefaultCommand extends WristCommand{
 
     @Override
     public void execute() {
-        if (Robot.isSimulation()) {
-            wrist.setPower(0);
-        }
-        else{
-            wrist.standInPlace();
-        }
+        wrist.standInPlace();
     }
 }
