939f93a41fff4279be2b41bfaec012c95d301f85
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-01-28 21:43:59 +0200

    yoav - cecked

--- src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/GetToTransferNotePosition.java
+++ src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/GetToTransferNotePosition.java
@@ -10,12 +10,12 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 public class GetToTransferNotePosition extends ParallelCommandGroup {
 
     public GetToTransferNotePosition() {
-//        super(
-//                new MoveElbowAndWrist(
-//                        ElbowConstants.ImportantPlaces.TRANSFER.ANGLE,
-//                        WristConstants.ImportantPlaces.TRANSFER.ANGLE
-//                ),
-//                new MovePivotToAngle(PivotConstants.ImportantPlaces.TRANSFER.ANGLE)
-//        );
+        super(
+                new MoveElbowAndWrist(
+                        ElbowConstants.ImportantPlaces.TRANSFER.ANGLE,
+                        WristConstants.ImportantPlaces.TRANSFER.ANGLE
+                ),
+                new MovePivotToAngle(PivotConstants.ImportantPlaces.TRANSFER.ANGLE)
+        );
     }
 }
--- src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterToAngle.java
+++ src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterToAngle.java
@@ -1,33 +1,33 @@
-//package edu.greenblitz.robotName.commands.GetNoteToSystem;
-//
-//import edu.greenblitz.robotName.commands.arm.MoveElbowAndWrist;
-//import edu.greenblitz.robotName.commands.shooter.pivot.MovePivotToAngle;
-//import edu.greenblitz.robotName.subsystems.arm.Elbow;
-//import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.ElbowConstants;
-//import edu.greenblitz.robotName.subsystems.arm.EndEffector.WristUtils.WristConstants;
-//import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
-//import edu.wpi.first.math.geometry.Rotation2d;
-//import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-//import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-//import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-//import edu.wpi.first.wpilibj2.command.WaitCommand;
-//
-//public class MoveShooterToAngle extends ConditionalCommand {
-//
-//	public MoveShooterToAngle(Rotation2d targetAngle) {
-//		super(
-//				new ParallelCommandGroup(
-//						new MoveElbowAndWrist(
-//								ElbowConstants.ImportantPlaces.SAFE.ANGLE,
-//								WristConstants.ImportantPlaces.TRANSFER.ANGLE
-//						),
-//						new SequentialCommandGroup(
-//								new WaitCommand(PivotConstants.NO_COLLISION_DELAY_SECONDS),
-//								new MovePivotToAngle(targetAngle)
-//						)
-//				),
-//				new MovePivotToAngle(targetAngle),
-//				() -> Elbow.getInstance().isInDangerZone());
-//	}
-//
-//}
+package edu.greenblitz.robotName.commands.GetNoteToSystem;
+
+import edu.greenblitz.robotName.commands.arm.MoveElbowAndWrist;
+import edu.greenblitz.robotName.commands.shooter.pivot.MovePivotToAngle;
+import edu.greenblitz.robotName.subsystems.arm.Elbow;
+import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.ElbowConstants;
+import edu.greenblitz.robotName.subsystems.arm.EndEffector.WristUtils.WristConstants;
+import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class MoveShooterToAngle extends ConditionalCommand {
+
+	public MoveShooterToAngle(Rotation2d targetAngle) {
+		super(
+				new ParallelCommandGroup(
+						new MoveElbowAndWrist(
+								ElbowConstants.ImportantPlaces.SAFE.ANGLE,
+								WristConstants.ImportantPlaces.TRANSFER.ANGLE
+						),
+						new SequentialCommandGroup(
+								new WaitCommand(PivotConstants.NO_COLLISION_DELAY_SECONDS),
+								new MovePivotToAngle(targetAngle)
+						)
+				),
+				new MovePivotToAngle(targetAngle),
+				() -> Elbow.getInstance().isInDangerZone());
+	}
+
+}
