6efe5e5e70daa751e8055ff1f9ff0268f199ad54
bossyShrimp68 <romypine007@gmail.com>
2024-01-29 17:35:11 +0200

    AMir - added pre amp position

--- src/main/java/edu/greenblitz/robotName/Field.java
+++ src/main/java/edu/greenblitz/robotName/Field.java
@@ -100,6 +100,7 @@ public class Field {
         public final static Pose2d SPEAKER_SCORE_POSITION = new Pose2d(new Translation2d(14.140,5.85),new Rotation2d());
         public final static Pose2d AMP_SCORE_POSITION = new Pose2d(new Translation2d(14.85,7.5),new Rotation2d());
         public final static Pose2d CLIMB_POSITION = new Pose2d(new Translation2d(12.733,3.602),new Rotation2d());
+        public final static Pose2d PRE_AMP_SCORE_POSITION = new Pose2d(new Translation2d(14.85,7.3),new Rotation2d());
     }
 
     public static class UpStairsFieldPositions{
