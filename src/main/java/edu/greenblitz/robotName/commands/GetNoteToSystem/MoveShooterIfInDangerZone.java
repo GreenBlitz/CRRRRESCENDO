5dedd2b373b478aa9f7dbf5d4d0f27634235adfb
YoniKiriaty <YoniKiriaty@gmail.com>
2024-01-30 23:19:33 +0200

    Yoni - changed it to sequential

--- src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterIfInDangerZone.java
+++ src/main/java/edu/greenblitz/robotName/commands/GetNoteToSystem/MoveShooterIfInDangerZone.java
@@ -1,18 +1,14 @@
 package edu.greenblitz.robotName.commands.GetNoteToSystem;
 
 import edu.greenblitz.robotName.commands.arm.MoveElbowAndWrist;
-import edu.greenblitz.robotName.commands.arm.WaitUntilArmIsSafe;
 import edu.greenblitz.robotName.commands.shooter.pivot.MovePivotToAngle;
 import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.ElbowConstants;
 import edu.greenblitz.robotName.subsystems.arm.EndEffector.WristUtils.WristConstants;
-import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 
-public class MoveShooterIfInDangerZone extends ParallelCommandGroup {
+public class MoveShooterIfInDangerZone extends SequentialCommandGroup {
 
     public MoveShooterIfInDangerZone(Rotation2d targetAngle) {
         super(
@@ -20,10 +16,7 @@ public class MoveShooterIfInDangerZone extends ParallelCommandGroup {
                         ElbowConstants.PresetPositions.SAFE.ANGLE,
                         WristConstants.PresetPositions.TRANSFER.ANGLE
                 ),
-                new SequentialCommandGroup(
-                        new WaitUntilArmIsSafe(),
-                        new MovePivotToAngle(targetAngle)
-                )
+                new MovePivotToAngle(targetAngle)
         );
     }
 }
