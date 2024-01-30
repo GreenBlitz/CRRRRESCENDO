444de6842b1aa600cd7ce0caac58976c6eca88e6
greenblitz4590 <greenblitz.4590@gmail.com>
2024-01-30 12:37:33 +0200

    yoav - neo elbow

--- src/main/java/edu/greenblitz/robotName/subsystems/arm/ElbowUtils/ElbowFactory.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/arm/ElbowUtils/ElbowFactory.java
@@ -2,12 +2,13 @@ package edu.greenblitz.robotName.subsystems.arm.ElbowUtils;
 
 import edu.greenblitz.robotName.RobotConstants;
 import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.MotorElbow.FalconElbow.FalconElbow;
+import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.MotorElbow.NeoElbow.NeoElbow;
 import edu.greenblitz.robotName.subsystems.arm.ElbowUtils.SimulationElbow.SimulationElbow;
 
 public class ElbowFactory {
     public static IElbow create() {
         return switch (RobotConstants.ROBOT_TYPE) {
-            case ROBOT_NAME -> new FalconElbow();
+            case ROBOT_NAME -> new NeoElbow();
             case REPLAY -> new ReplayElbow();
             case SIMULATION -> new SimulationElbow();
         };
