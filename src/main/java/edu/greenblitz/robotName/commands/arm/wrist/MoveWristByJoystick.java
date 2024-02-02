6a348fcfc0e5a2a30157e708e97d9bb4fd186db5
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-02 15:31:17 +0200

    yoav - joystick

---
+++ src/main/java/edu/greenblitz/robotName/commands/arm/wrist/MoveWristByJoystick.java
@@ -0,0 +1,20 @@
+package edu.greenblitz.robotName.commands.arm.wrist;
+
+import edu.greenblitz.robotName.RobotConstants;
+import edu.greenblitz.robotName.utils.hid.SmartJoystick;
+
+public class MoveWristByJoystick extends WristCommand{
+
+    private SmartJoystick joystick;
+
+    public MoveWristByJoystick(SmartJoystick joystick){
+        super();
+        this.joystick = joystick;
+    }
+
+    @Override
+    public void execute() {
+        double power = joystick.getAxisValue(SmartJoystick.Axis.RIGHT_X) * RobotConstants.General.SAFETY_POWER_CONVERSION_FACTOR;
+        wrist.setPower(power);
+    }
+}
