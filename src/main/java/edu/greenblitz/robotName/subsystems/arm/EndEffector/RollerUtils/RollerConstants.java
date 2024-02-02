0ad69c972273ce161ca49acc4c7c9f1c754c1f32
RJ <ranelj2008@gmail.com>
2024-02-02 13:52:05 +0200

    Ran - hopefully final CR

--- src/main/java/edu/greenblitz/robotName/subsystems/arm/EndEffector/RollerUtils/RollerConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/arm/EndEffector/RollerUtils/RollerConstants.java
@@ -4,7 +4,7 @@ public class RollerConstants {
 
 	public static final double ROLL_FORWARD_POWER = 1;
 	public static final double ROLL_BACKWARD_POWER = -1;
-	public static final double TIME_UNTIL_NOTE_EXIT_ARM = 0.5;
+	public static final double TIME_UNTIL_NOTE_EXIT_ROLLER = 0.5;
 
 
 }
