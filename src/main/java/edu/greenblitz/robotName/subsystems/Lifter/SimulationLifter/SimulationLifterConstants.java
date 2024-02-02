0e6b562dbffea900d69d9b014b9b18826d87ea6b
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-02 16:44:01 +0200

    Yoni - returned gearRatio

--- src/main/java/edu/greenblitz/robotName/subsystems/Lifter/SimulationLifter/SimulationLifterConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Lifter/SimulationLifter/SimulationLifterConstants.java
@@ -9,7 +9,7 @@ public class SimulationLifterConstants {
 
     public static final int NUMBER_OF_MOTORS = 1;
 
-    public static final double GEAR_RATIO = 300;
+    public static final double GEAR_RATIO = 30;
 
     public static final double MOMENT_OF_INERTIA = 0.03;
 
