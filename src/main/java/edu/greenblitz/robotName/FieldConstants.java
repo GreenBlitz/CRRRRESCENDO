3d9e9dc386cd7a3dedea492e54975b75990d2013
unknown <romypine007@gmail.com>
2024-02-03 14:09:22 +0200

    Romy - fixed aliiance

--- src/main/java/edu/greenblitz/robotName/FieldConstants.java
+++ src/main/java/edu/greenblitz/robotName/FieldConstants.java
@@ -1,9 +1,11 @@
 package edu.greenblitz.robotName;
 
 
+import edu.greenblitz.robotName.utils.FMSUtils;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.DriverStation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,21 +21,39 @@ public class FieldConstants {
 
     public final static double FIELD_WIDTH = 8.0137;
 
-    public static final Translation3d MIDDLE_OF_SPEAKER_POSITION = new Translation3d(15.24, 5.9944, 2.045);
+    public static final Translation3d MIDDLE_OF_RED_SPEAKER_POSITION = new Translation3d(15.24, 5.9944, 2.045);
+
+    public static final Translation3d MIDDLE_OF_BLUE_SPEAKER_POSITION = new Translation3d(0,0,0);
+
+    public static final Translation3d MIDDLE_OF_SPEAKER = FMSUtils.isRedAlliance() ? MIDDLE_OF_RED_SPEAKER_POSITION : MIDDLE_OF_BLUE_SPEAKER_POSITION;
 
     public static class FieldZones {
 
         public static final List<Pair<Translation2d, Translation2d>> RESTRICTED_BOUNDS = new ArrayList<>();
 
-        public static final Pair<Translation2d, Translation2d> STAGE_ZONE = new Pair<>(
+        public static final Pair<Translation2d, Translation2d> RED_STAGE_ZONE = new Pair<>(
                 new Translation2d(13.8, 3),
                 new Translation2d(12.4, 4.95)
         );
 
-        public static final Pair<Translation2d, Translation2d> ENEMY_FEEDER_ZONE = new Pair<>(
+        public static final Pair<Translation2d, Translation2d> BLUE_STAGE_ZONE = new Pair<>(
+                new Translation2d(13.8, 3),
+                new Translation2d(12.4, 4.95)
+        );
+
+        public static final Pair<Translation2d, Translation2d> RED_FEEDER_ZONE = new Pair<>(
                 new Translation2d(12.5, 7.4),
                 new Translation2d(15, 15)
         );
+
+        public static final Pair<Translation2d, Translation2d> BLUE_FEEDER_ZONE = new Pair<>(
+                new Translation2d(12.5, 7.4),
+                new Translation2d(15, 15)
+        );
+
+        public static final Pair<Translation2d, Translation2d> ENEMY_FEEDER_ZONE = FMSUtils.isRedAlliance() ? BLUE_FEEDER_ZONE : RED_FEEDER_ZONE;
+
+        public static final Pair<Translation2d, Translation2d> STAGE_ZONE = FMSUtils.isRedAlliance() ? RED_STAGE_ZONE : BLUE_STAGE_ZONE;
 
         static {
             RESTRICTED_BOUNDS.add(STAGE_ZONE);
--- src/main/java/edu/greenblitz/robotName/utils/FMSUtils.java
+++ src/main/java/edu/greenblitz/robotName/utils/FMSUtils.java
@@ -3,13 +3,13 @@ package edu.greenblitz.robotName.utils;
 import edu.wpi.first.wpilibj.DriverStation;
 import org.littletonrobotics.junction.Logger;
 
-public class FMSUtils {
+import java.security.PublicKey;
 
+public class FMSUtils {
 
     private static final DriverStation.Alliance DEFAULT_ALLIANCE = DriverStation.Alliance.Blue;
 
     public static DriverStation.Alliance getAlliance() {
-
         if (DriverStation.getAlliance().isPresent()) {
             Logger.recordOutput("FMS/Alliance", DriverStation.getAlliance().get().name());
             return DriverStation.getAlliance().get();
@@ -18,4 +18,7 @@ public class FMSUtils {
         return DEFAULT_ALLIANCE;
     }
 
+    public static boolean isRedAlliance() {
+        return getAlliance() == DriverStation.Alliance.Red;
+    }
 }
