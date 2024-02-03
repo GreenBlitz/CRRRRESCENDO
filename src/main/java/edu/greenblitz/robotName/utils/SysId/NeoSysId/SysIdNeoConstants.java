22ce31dbc3fbbd5b023dad431c2184ed93b7eac3
YHerm <0549405422.yh@gmail.com>
2024-02-03 21:02:23 +0200

    yoav - added sysid neo

--- src/main/java/edu/greenblitz/robotName/utils/SysId/NeoSysId/SysIdNeoConstants.java
+++ src/main/java/edu/greenblitz/robotName/utils/SysId/NeoSysId/SysIdNeoConstants.java
@@ -1,4 +1,21 @@
 package edu.greenblitz.robotName.utils.SysId.NeoSysId;
 
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import edu.greenblitz.robotName.utils.motors.GBSparkMax;
+
 public class SysIdNeoConstants {
+
+    public static final double DYNAMIC_VOLTAGE = 4;
+
+    public static final int MOTOR_ID = 187;
+
+    public static final double FORWARD_LIMIT = 20;
+
+    public static final double BACKWARD_LIMIT = -20;
+
+    public static final boolean ENABLE_FORWARD_LIMIT = true;
+
+    public static final boolean ENABLE_BACKWARD_LIMIT = true;
+
+
 }
