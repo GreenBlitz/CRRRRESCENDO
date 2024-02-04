60239e309f3caa2869fedaeaf815e50c723a4641
RJ <ranelj2008@gmail.com>
2024-02-04 20:51:38 +0200

    Ran - Roy's CR

--- src/main/java/edu/greenblitz/robotName/commands/arm/roller/RollIn.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/roller/RollIn.java
@@ -1,6 +1,6 @@
 package edu.greenblitz.robotName.commands.arm.roller;
 
-public class RunRoller extends RollerCommand{
+public class RollIn extends RollerCommand{
     @Override
     public void initialize() {
         roller.rollIn();
