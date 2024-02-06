66cde39f9542864639f095f744f0f2d8786c618d
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-06 13:30:14 +0200

    yoav- fix

--- src/main/java/edu/greenblitz/robotName/Robot.java
+++ src/main/java/edu/greenblitz/robotName/Robot.java
@@ -6,7 +6,6 @@ import com.pathplanner.lib.pathfinding.LocalADStar;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 import edu.greenblitz.robotName.subsystems.Intake.Intake;
 import edu.greenblitz.robotName.subsystems.Lifter.Lifter;
-import edu.greenblitz.robotName.subsystems.ArmShooterMechanism.ArmShooterMechanism;
 import edu.greenblitz.robotName.subsystems.shooter.Pivot.Pivot;
 import edu.greenblitz.robotName.subsystems.arm.elbow.Elbow;
 import edu.greenblitz.robotName.subsystems.arm.roller.Roller;
@@ -66,7 +65,6 @@ public class Robot extends LoggedRobot {
         Elbow.init();
         Wrist.init();
         Roller.init();
-        ArmShooterMechanism.init();
 
         Lifter.init();
         Intake.init();
@@ -81,7 +79,6 @@ public class Robot extends LoggedRobot {
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
         RoborioUtils.updateCurrentCycleTime();
-        ArmShooterMechanism.getInstance().periodic();
     }
 
     private void initializeAutonomousBuilder() {
