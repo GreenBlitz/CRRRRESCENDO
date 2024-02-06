2a2c94cb03e602e9618473ccc9b13d2c55d0bcd0
hillelv <hillel.vider@gmail.com>
2024-02-06 19:41:06 +0200

    Yoni - deleted MOI

--- src/main/java/edu/greenblitz/robotName/subsystems/Lifter/SimulationLifter/SimulationLifterConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/Lifter/SimulationLifter/SimulationLifterConstants.java
@@ -11,8 +11,6 @@ public class SimulationLifterConstants {
 
     public static final double GEAR_RATIO = 30;
 
-    public static final double MOMENT_OF_INERTIA = 0.03;
-
     public static final double DEBOUNCE_TIME_FOR_SWITCH = 0.1;
 
     public static final ProfiledPIDController SIMULATION_PID = new ProfiledPIDController(3, 0, 0, new TrapezoidProfile.Constraints(5, 10));
