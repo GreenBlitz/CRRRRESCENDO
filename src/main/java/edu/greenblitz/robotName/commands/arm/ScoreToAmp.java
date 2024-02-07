c638a3b5401e404782295fb4c9920cdde99fe4b4
RJ <ranelj2008@gmail.com>
2024-02-07 21:15:35 +0200

    Ran -blah blah

---
+++ src/main/java/edu/greenblitz/robotName/commands/arm/ScoreToAmp.java
@@ -0,0 +1,17 @@
+package edu.greenblitz.robotName.commands.arm;
+import edu.greenblitz.robotName.commands.arm.roller.RunRollerUntilObjectOut;
+import edu.greenblitz.robotName.subsystems.arm.elbow.ElbowConstants;
+import edu.greenblitz.robotName.subsystems.arm.wrist.WristConstants;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class ScoreToAmp extends SequentialCommandGroup {
+    public ScoreToAmp(){
+        super(
+                new MoveElbowAndWrist(
+                        WristConstants.PresetPositions.SCORE.ANGLE,
+                        ElbowConstants.PresetPositions.SCORE.ANGLE
+                ),
+                new RunRollerUntilObjectOut()
+        );
+    }
+}
