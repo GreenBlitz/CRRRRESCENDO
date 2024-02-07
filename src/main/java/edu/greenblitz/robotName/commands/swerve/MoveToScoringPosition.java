d4d3b2bd3aaf77b6105aec86415d04dc3f17b48c
YHerm <0549405422.yh@gmail.com>
2024-02-07 13:00:33 +0200

    yoav - changed to real condition

--- src/main/java/edu/greenblitz/robotName/commands/swerve/MoveToScoringPosition.java
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/MoveToScoringPosition.java
@@ -1,5 +1,6 @@
 package edu.greenblitz.robotName.commands.swerve;
 
+import edu.greenblitz.robotName.ScoringModeSelector;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 
@@ -7,7 +8,7 @@ import java.util.function.BooleanSupplier;
 
 public class MoveToScoringPosition extends ConditionalCommand {
 
-    public MoveToScoringPosition(BooleanSupplier condition) {
-        super( new MoveToSpeaker(), new MoveToAmp(), condition);
+    public MoveToScoringPosition() {
+        super( new MoveToSpeaker(), new MoveToAmp(), ScoringModeSelector::isSpeakerMode);
     }
 }
