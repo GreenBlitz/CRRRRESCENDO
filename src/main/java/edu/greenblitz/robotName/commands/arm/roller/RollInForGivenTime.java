f8593197483f91733bc0b072eae8ebfc23ac1a8f
Amir <75812980+aaa-png@users.noreply.github.com>
2024-02-08 18:29:04 +0200

    Update src/main/java/edu/greenblitz/robotName/commands/arm/roller/RollInForGivenTime.java

--- src/main/java/edu/greenblitz/robotName/commands/arm/roller/RollInForGivenTime.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/roller/RollInForGivenTime.java
@@ -3,7 +3,7 @@ package edu.greenblitz.robotName.commands.arm.roller;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class RollInForGivenTime extends ParallelRaceGroup {
+public class RollInForAGivenTime extends ParallelRaceGroup {
     public RollInForGivenTime(double seconds){
         super(
                 new RollIn(),
