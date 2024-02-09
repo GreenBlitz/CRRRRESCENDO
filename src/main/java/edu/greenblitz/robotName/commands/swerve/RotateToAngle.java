a52957fe99ef81fa27d136c246f533334e27c12a
hillelv <hillel.vider@gmail.com>
2024-02-09 10:32:23 +0200

    hillel- we no merging sout

--- src/main/java/edu/greenblitz/robotName/commands/swerve/RotateToAngle.java
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/RotateToAngle.java
@@ -22,7 +22,6 @@ public class RotateToAngle extends SwerveCommand {
 
     @Override
     public void initialize() {
-        System.out.println("rotate init");
         ChassisConstants.ROTATION_PID_CONTROLLER.setSetpoint(angleSetPoint.get().getRadians());
     }
 
