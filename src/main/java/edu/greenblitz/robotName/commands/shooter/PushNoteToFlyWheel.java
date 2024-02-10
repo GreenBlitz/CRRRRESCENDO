bc71902b20db4bd1e75fdbb4d9db83c10684888e
hillelv <hillel.vider@gmail.com>
2024-02-10 22:36:27 +0200

    hillel- funnel will run instead flywheel running twice

--- src/main/java/edu/greenblitz/robotName/commands/shooter/PushNoteToFlyWheel.java
+++ src/main/java/edu/greenblitz/robotName/commands/shooter/PushNoteToFlyWheel.java
@@ -2,6 +2,7 @@ package edu.greenblitz.robotName.commands.shooter;
 
 import edu.greenblitz.robotName.commands.shooter.flyWheel.RunFlyWheelByApropriateVelocity;
 import edu.greenblitz.robotName.commands.shooter.flyWheel.StopFlyWheel;
+import edu.greenblitz.robotName.commands.shooter.funnel.runByPowerUntilCondition.ForwardRunFunnelUntilObjectOut;
 import edu.greenblitz.robotName.subsystems.shooter.FlyWheel.FlyWheelConstants;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -11,10 +12,8 @@ public class PushNoteToFlyWheel extends SequentialCommandGroup {
 
     public PushNoteToFlyWheel() {
         super(
-                new ParallelCommandGroup(
-                        new WaitCommand(FlyWheelConstants.DELAY_SECONDS_UNTIL_EXIT),
-                        new RunFlyWheelByApropriateVelocity()
-                ),
+                new ForwardRunFunnelUntilObjectOut(),
+                new WaitCommand(FlyWheelConstants.DELAY_SECONDS_UNTIL_EXIT),
                 new StopFlyWheel()
         );
     }
