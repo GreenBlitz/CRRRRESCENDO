40d2fce51c0c62d839d3492afcf46b951c07b96e
greenblitz4590 <greenblitz4590@gmail.com>
2024-02-10 22:46:50 +0200

    yova

--- src/main/java/edu/greenblitz/robotName/subsystems/shooter/Pivot/SimulationPivot/SimulationPivotConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/shooter/Pivot/SimulationPivot/SimulationPivotConstants.java
@@ -9,8 +9,10 @@ public class SimulationPivotConstants {
 
     public static final int NUMBER_OF_MOTORS = 1;
 
-    public static final double GEAR_RATIO = 1 / FalconPivotConstants.FEEDBACK_CONFIGS.SensorToMechanismRatio;
+    public static final double GEAR_RATIO = 1 / 0.0328;
 
-    public static final PIDObject SIMULATION_PID = new PIDObject().withKp(0.5).withKd(0).withMaxPower(1);
+    //public static final double GEAR_RATIO = 1 / FalconPivotConstants.FEEDBACK_CONFIGS.SensorToMechanismRatio; TODO - NEED TO CALCULATE PID FOR THIS
+
+    public static final PIDObject SIMULATION_PID = new PIDObject().withKp(0.3).withKd(0).withMaxPower(1);
     
 }
