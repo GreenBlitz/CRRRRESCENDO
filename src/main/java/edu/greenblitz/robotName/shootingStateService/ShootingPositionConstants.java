eb0b0083932b16ba225367fc474978c28be66f1e
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-10 23:46:49 +0200

    yoav - cr changes

--- src/main/java/edu/greenblitz/robotName/shootingStateService/ShootingPositionConstants.java
+++ src/main/java/edu/greenblitz/robotName/shootingStateService/ShootingPositionConstants.java
@@ -9,11 +9,11 @@ public class ShootingPositionConstants {
     
     public static final Translation2d CENTER_OF_SHOOTING_ZONE = FieldConstants.MIDDLE_OF_SPEAKER_POSITION.toTranslation2d();
 
-    public static final double LEGAL_SHOOTING_ZONE_RADIUS_IN_METERS = 5;
+    public static final double SHOOTING_ZONE_RADIUS_IN_METERS = 5;
 
     public static ShootingZone LEGAL_SHOOTING_ZONE = new ShootingZone(
             CENTER_OF_SHOOTING_ZONE,
-            LEGAL_SHOOTING_ZONE_RADIUS_IN_METERS,
+            SHOOTING_ZONE_RADIUS_IN_METERS,
             FieldConstants.FieldZones.RESTRICTED_BOUNDS
     );
 }
--- src/main/java/edu/greenblitz/robotName/subsystems/shooter/Pivot/SimulationPivot/SimulationPivotConstants.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/shooter/Pivot/SimulationPivot/SimulationPivotConstants.java
@@ -9,9 +9,7 @@ public class SimulationPivotConstants {
 
     public static final int NUMBER_OF_MOTORS = 1;
 
-    public static final double GEAR_RATIO = 1 / 0.0328;
-
-    //public static final double GEAR_RATIO = 1 / FalconPivotConstants.FEEDBACK_CONFIGS.SensorToMechanismRatio; TODO - NEED TO CALCULATE PID FOR THIS
+    public static final double GEAR_RATIO = 1 / 0.0328; //REAL VALUE - 1 / FalconPivotConstants.FEEDBACK_CONFIGS.SensorToMechanismRatio
 
     public static final PIDObject SIMULATION_PID = new PIDObject().withKp(0.3).withKd(0).withMaxPower(1);
     
