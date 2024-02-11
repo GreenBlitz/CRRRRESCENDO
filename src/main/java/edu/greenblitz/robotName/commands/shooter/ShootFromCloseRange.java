7765c6e22ee51aedc93c28e458294825abc5e445
greenblitz4590 <greenblitz4590@gmail.com>
2024-02-11 22:32:09 +0200

    Romy - fixed my own cr requests

--- src/main/java/edu/greenblitz/robotName/commands/shooter/ShootFromCloseRange.java
+++ src/main/java/edu/greenblitz/robotName/commands/shooter/ShootFromCloseRange.java
@@ -1,18 +1,19 @@
 package edu.greenblitz.robotName.commands.shooter;
 
-import edu.greenblitz.robotName.commands.shooter.pivot.MovePivotToAngle;
 import edu.greenblitz.robotName.commands.swerve.RotateToAngle;
 import edu.greenblitz.robotName.subsystems.shooter.Pivot.PivotConstants;
 import edu.greenblitz.robotName.subsystems.swerve.Chassis.ChassisConstants;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class ShootFromCloseRange extends SequentialCommandGroup {
     public ShootFromCloseRange(){
         super(
-                new RotateToAngle(ChassisConstants.CLOSE_RANGE_SHOOTING_ANGLE),
-                new MoveShooterToAngle(PivotConstants.CLOSE_RANGE_SHOOTING_ANGLE),
-                new DefaultShooting()
+                new ParallelCommandGroup(
+                        new RotateToAngle(ChassisConstants.CLOSE_RANGE_SHOOTING_ANGLE),
+                        new MoveShooterToAngle(PivotConstants.CLOSE_RANGE_SHOOTING_ANGLE)
+                ),
+                new ShootFromInFunnel()
         );
     }
 }
--- src/main/java/edu/greenblitz/robotName/commands/shooter/ShootFromInFunnel.java
+++ src/main/java/edu/greenblitz/robotName/commands/shooter/ShootFromInFunnel.java
@@ -3,8 +3,8 @@ package edu.greenblitz.robotName.commands.shooter;
 import edu.greenblitz.robotName.commands.shooter.flyWheel.RunFlyWheelByVelocityConstant;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class DefaultShooting extends SequentialCommandGroup {
-    public DefaultShooting(){
+public class ShootFromInFunnel extends SequentialCommandGroup {
+    public ShootFromInFunnel(){
         super(
                 new RunFlyWheelByVelocityConstant(),
                 new PushNoteToFlyWheel()
