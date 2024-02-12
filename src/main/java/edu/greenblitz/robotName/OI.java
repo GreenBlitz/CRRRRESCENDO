c59c4d613b68d7af1e444047c52aa04472a39ee3
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-12 20:31:03 +0200

    Ran - switched button A to score to amp

--- src/main/java/edu/greenblitz/robotName/OI.java
+++ src/main/java/edu/greenblitz/robotName/OI.java
@@ -3,6 +3,7 @@ package edu.greenblitz.robotName;
 import edu.greenblitz.robotName.commands.PanicMode;
 import edu.greenblitz.robotName.commands.arm.MoveElbowAndWrist;
 import edu.greenblitz.robotName.commands.arm.MoveElbowAndWristToSafe;
+import edu.greenblitz.robotName.commands.arm.ScoreToAmp;
 import edu.greenblitz.robotName.commands.arm.roller.MoveNoteToMiddleOfRoller;
 import edu.greenblitz.robotName.commands.arm.roller.RunByPower.RunRollerCounterClockwiseUntilNoteIsInside;
 import edu.greenblitz.robotName.commands.arm.roller.RunRollerByJoystick;
@@ -87,7 +88,7 @@ public class OI {
     public void initButtons() {
         secondJoystick.START.whileTrue(new PanicMode());
         secondJoystick.BACK.whileTrue(new ToggleScoringMode());
-        secondJoystick.A.whileTrue(new MoveElbowAndWrist(ElbowConstants.PresetPositions.SCORE.ANGLE, WristConstants.PresetPositions.SCORE.ANGLE));
+        secondJoystick.A.whileTrue(new ScoreToAmp());
         secondJoystick.B.whileTrue(new MoveElbowAndWristToSafe());
         secondJoystick.X.whileTrue(new MoveShooterToAngle(PivotConstants.PresetPositions.PICK_UP.ANGLE));
         secondJoystick.Y.whileTrue(new MoveShooterToAngle(PivotConstants.PresetPositions.TRANSFER.ANGLE));
