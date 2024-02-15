da277123e9131dc2698687862d90943a3ec1fdf9
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-15 14:51:52 +0200

    Yoni - removed imports

--- src/main/java/edu/greenblitz/robotName/Field.java
+++ src/main/java/edu/greenblitz/robotName/Field.java
@@ -1,26 +1,20 @@
 package edu.greenblitz.robotName;
 
-import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.*;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 
 public class Field {
-	
+
 	/**
 	 * gets pose, returns it fitted to the other alliance ("mirrored") and rotated by 180 degrees.
 	 */
 	public static final boolean IS_AT_HADREAM_MIRRORD = false;
-	
+
 	public static Pose2d mirrorPositionToOtherSide(Pose2d pose) {
 		Pose2d mirroredPose = new Pose2d(
 				FieldConstants.FIELD_LENGTH - pose.getX(),
@@ -29,7 +23,7 @@ public class Field {
 		);
 		return mirroredPose;
 	}
-	
+
 	/**
 	 * gets pose[], returns it fitted to the other alliance ("mirrored") and rotated by 180 degrees.
 	 */
@@ -40,7 +34,7 @@ public class Field {
 		}
 		return mirroredPoses;
 	}
-	
+
 	public static class Apriltags {
 
 		public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT;
@@ -53,9 +47,9 @@ public class Field {
 			}
 		}
 	}
-	
+
 	public static class PlacementLocations {
-		
+
 		// the locations of the grid plus half of the robot length.
 		private static final Pose2d[] LOCATIONS_ON_BLUE_SIDE = {
 				new Pose2d(new Translation2d(1.43 + (0.5 * RobotConstants.Syncopa.ROBOT_LENGTH_IN_METERS) + RobotConstants.Syncopa.BUMPER_LENGTH + 0.45, 0.508 + 0.36), new Rotation2d(Math.PI)),
@@ -68,65 +62,65 @@ public class Field {
 				new Pose2d(new Translation2d(1.43 + (0.5 * RobotConstants.Syncopa.ROBOT_LENGTH_IN_METERS) + RobotConstants.Syncopa.BUMPER_LENGTH + 0.45, 4.420 + 0.36), new Rotation2d(Math.PI)),
 				new Pose2d(new Translation2d(1.43 + (0.5 * RobotConstants.Syncopa.ROBOT_LENGTH_IN_METERS) + RobotConstants.Syncopa.BUMPER_LENGTH + 0.45, 4.978 + 0.36), new Rotation2d(Math.PI))
 		};
-		
+
 		public static Pose2d[] getLocationsOnBlueSide() {
 			return LOCATIONS_ON_BLUE_SIDE;
 		}
-		
+
 		public static Pose2d[] getLocationsOnRedSide() {
 			return mirrorPositionsToOtherSide(LOCATIONS_ON_BLUE_SIDE);
 		}
-		
+
 		private static final HashSet<Integer> CUBE_INDICES = new HashSet<>(Arrays.asList(1, 4, 7));
-		
+
 		public static boolean isGridPositionIDofCube(int index) {
 			return CUBE_INDICES.contains(index);
 		}
-		
+
 		public static boolean isGridPositionIDofCone(int index) {
 			return !isGridPositionIDofCube(index);
 		}
-		
+
 		public static final Pose2d OUT_PRE_BALANCE_BLUE = new Pose2d(5.40, 2.67, new Rotation2d(Math.PI));
 	}
-	
+
 	public static class ScoringPositions {
-		
+
 		public final static Pose2d RED_SPEAKER_SCORE_POSITION = new Pose2d(new Translation2d(14.140, 5.85), new Rotation2d());
-		
+
 		public final static Pose2d RED_SCORE_POSITION = new Pose2d(new Translation2d(14.85, 7.5), new Rotation2d());
-		
+
 		public final static Pose2d RED_UPPER_CLIMB_POSITION = new Pose2d(new Translation2d(12.2, 5), new Rotation2d());
-		
+
 		public final static Pose2d RED_LOWER_CLIMB_POSITION = new Pose2d(new Translation2d(12.2, 3.2), new Rotation2d());
-		
+
 		public final static Pose2d RED_MID_CLIMB_POSITION = new Pose2d(new Translation2d(10.6, 4), new Rotation2d());
-		
+
 		public final static Pose2d RED_PRE_AMP_SCORE_POSITION = new Pose2d(new Translation2d(14.85, 7.3), new Rotation2d());
-		
+
 		public final static Pose2d RED_AMP_SCORE_POSITION = new Pose2d(new Translation2d(14.85, 7.5), new Rotation2d());
-		
+
 		public final static Pose2d BLUE_SPEAKER_SCORE_POSITION = mirrorPositionToOtherSide(RED_SPEAKER_SCORE_POSITION);
-		
+
 		public final static Pose2d BLUE_SCORE_POSITION = mirrorPositionToOtherSide(RED_SCORE_POSITION);
-		
+
 		public final static Pose2d BLUE_UPPER_CLIMB_POSITION = mirrorPositionToOtherSide(RED_UPPER_CLIMB_POSITION);
-		
+
 		public final static Pose2d BLUE_LOWER_CLIMB_POSITION = mirrorPositionToOtherSide(RED_LOWER_CLIMB_POSITION);
-		
+
 		public final static Pose2d BLUE_MID_CLIMB_POSITION = mirrorPositionToOtherSide(RED_MID_CLIMB_POSITION);
-		
+
 		public final static Pose2d BLUE_PRE_AMP_SCORE_POSITION = mirrorPositionToOtherSide(RED_PRE_AMP_SCORE_POSITION);
-		
+
 		public final static Pose2d BLUE_AMP_SCORE_POSITION = mirrorPositionToOtherSide(RED_AMP_SCORE_POSITION);
 	}
-	
+
 	public static class UpStairsFieldPositions {
-		
+
 		public final static Pose2d SPEAKER_SCORE_POSITION = new Pose2d(new Translation2d(), new Rotation2d());
-		
+
 		public final static Pose2d AMP_SCORE_POSITION = new Pose2d(new Translation2d(), new Rotation2d());
-		
+
 		public final static Pose2d CLIMB_POSITION = new Pose2d(new Translation2d(), new Rotation2d());
 	}
 }
