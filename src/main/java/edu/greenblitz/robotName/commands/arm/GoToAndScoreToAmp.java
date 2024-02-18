718c0cee11627bb9d15d8d687b4c010bd0634bef
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-18 16:13:34 +0200

    Yoni - im very sdupid

--- src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
@@ -1,14 +1,14 @@
 package edu.greenblitz.robotName.commands.arm;
 
 import edu.greenblitz.robotName.commands.swerve.MoveToAmp;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class GoToAndScoreToAmp extends ParallelCommandGroup {
+public class GoToAndScoreToAmp extends SequentialCommandGroup {
 
     public GoToAndScoreToAmp() {
         super(
-                new ScoreToAmp(),
-                new MoveToAmp()
+                new MoveToAmp(),
+                new ScoreToAmp()
         );
     }
 }
