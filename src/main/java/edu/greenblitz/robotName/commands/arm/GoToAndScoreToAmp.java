379a3f05c0adc0e3c964de3c97a4396d6fca9ef8
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-18 16:16:14 +0200

    Yoni - fixes scoring

--- src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
@@ -1,14 +1,24 @@
 package edu.greenblitz.robotName.commands.arm;
 
+import edu.greenblitz.robotName.commands.arm.roller.runByPower.RunRollerClockwiseUntilObjectIsOut;
 import edu.greenblitz.robotName.commands.swerve.MoveToAmp;
+import edu.greenblitz.robotName.subsystems.arm.elbow.ElbowConstants;
+import edu.greenblitz.robotName.subsystems.arm.wrist.WristConstants;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class GoToAndScoreToAmp extends SequentialCommandGroup {
 
     public GoToAndScoreToAmp() {
         super(
-                new MoveToAmp(),
-                new ScoreToAmp()
+                new ParallelCommandGroup(
+                        new MoveToAmp(),
+                        new MoveElbowAndWrist(
+                                ElbowConstants.PresetPositions.SCORE.ANGLE,
+                                WristConstants.PresetPositions.SCORE.ANGLE
+                        )
+                ),
+                new RunRollerClockwiseUntilObjectIsOut()
         );
     }
 }
