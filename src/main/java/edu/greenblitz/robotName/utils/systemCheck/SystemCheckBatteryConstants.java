d6315cf19006d4275e6c7676c276bf211eacf178
hillelv <hillel.vider@gmail.com>
2024-02-18 17:46:30 +0200

    hillel - real battery constants

--- src/main/java/edu/greenblitz/robotName/utils/systemCheck/SystemCheckBatteryConstants.java
+++ src/main/java/edu/greenblitz/robotName/utils/systemCheck/SystemCheckBatteryConstants.java
@@ -1,8 +1,8 @@
 package edu.greenblitz.robotName.utils.systemCheck;
 
 public class SystemCheckBatteryConstants {
-    public static final double MAX_INNER_BATTERY_RESISTANCE = 0;
-    public static final double MAX_VOLTAGE_DROP = 0;
+    public static final double MAX_INNER_BATTERY_RESISTANCE = 0.02;
+    public static final double MAX_VOLTAGE_DROP = 4;
 
     public static final double DEFAULT_STARTING_VOLTAGE = 13;
 }
