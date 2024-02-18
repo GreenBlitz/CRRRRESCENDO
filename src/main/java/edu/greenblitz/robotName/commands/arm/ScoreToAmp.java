ddf938d9ff76b1514fc47084d1d6aa6fb725efa2
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-18 15:30:18 +0200

    Yoni - returned amp scoring

--- src/main/java/edu/greenblitz/robotName/commands/arm/ScoreToAmp.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/ScoreToAmp.java
@@ -1,19 +1,19 @@
 package edu.greenblitz.robotName.commands.arm;
 
 import edu.greenblitz.robotName.commands.arm.roller.runByPower.RunRollerClockwiseUntilObjectIsOut;
-import edu.greenblitz.robotName.commands.swerve.MoveToAmp;
 import edu.greenblitz.robotName.subsystems.arm.elbow.ElbowConstants;
 import edu.greenblitz.robotName.subsystems.arm.wrist.WristConstants;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class GoToAndScoreToAmp extends ParallelCommandGroup {
+public class ScoreToAmp extends SequentialCommandGroup {
 
-    public GoToAndScoreToAmp() {
+    public ScoreToAmp() {
         super(
-                new ScoreToAmp(),
-                new MoveToAmp()
-
+                new MoveElbowAndWrist(
+                        ElbowConstants.PresetPositions.SCORE.ANGLE,
+                        WristConstants.PresetPositions.SCORE.ANGLE
+                ),
+                new RunRollerClockwiseUntilObjectIsOut()
         );
     }
 }
