f474714fd472a2039de79fd7ae39a7ff214545ca
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-18 15:30:50 +0200

    Yoni - Ctrl + Alt + L

--- src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/GoToAndScoreToAmp.java
@@ -1,11 +1,7 @@
 package edu.greenblitz.robotName.commands.arm;
 
-import edu.greenblitz.robotName.commands.arm.roller.runByPower.RunRollerClockwiseUntilObjectIsOut;
 import edu.greenblitz.robotName.commands.swerve.MoveToAmp;
-import edu.greenblitz.robotName.subsystems.arm.elbow.ElbowConstants;
-import edu.greenblitz.robotName.subsystems.arm.wrist.WristConstants;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class GoToAndScoreToAmp extends ParallelCommandGroup {
 
