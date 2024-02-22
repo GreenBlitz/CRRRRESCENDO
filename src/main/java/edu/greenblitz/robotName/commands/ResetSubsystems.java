c1987469b0b46704f231bcf32a01c4135c64401a
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-22 17:14:33 +0200

    Yoni - added whole reset

---
+++ src/main/java/edu/greenblitz/robotName/commands/ResetSubsystems.java
@@ -0,0 +1,32 @@
+package edu.greenblitz.robotName.commands;
+
+import edu.greenblitz.robotName.Robot;
+import edu.greenblitz.robotName.subsystems.arm.wrist.Wrist;
+import edu.greenblitz.robotName.subsystems.shooter.pivot.Pivot;
+import edu.greenblitz.robotName.subsystems.shooter.pivot.PivotConstants;
+import edu.greenblitz.robotName.subsystems.swerve.chassis.SwerveChassis;
+import edu.greenblitz.robotName.utils.GBCommand;
+import edu.greenblitz.robotName.subsystems.arm.elbow.Elbow;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class ResetSubsystems extends GBCommand {
+
+    @Override
+    public void initialize() {
+        resetSubsystems();
+    }
+
+    private void resetSubsystems() {
+        Pivot.getInstance().moveToAngle(PivotConstants.PresetPositions.SAFE.ANGLE);
+        Elbow.getInstance().moveToAngle(Rotation2d.fromRadians(0));
+        SwerveChassis.getInstance().resetWheels();
+        Wrist.getInstance().moveToAngle(Rotation2d.fromRadians(0));
+    }
+
+    @Override
+    public void execute() {
+        if (Robot.isSimulation()) {
+            resetSubsystems();
+        }
+    }
+}
--- src/main/java/edu/greenblitz/robotName/subsystems/swerve/chassis/SwerveChassis.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/swerve/chassis/SwerveChassis.java
@@ -536,6 +536,13 @@ public class SwerveChassis extends GBSubsystem implements ISwerveChassis {
 		doVision = true;
 	}
 
+	public void resetWheels() {
+		frontLeft.rotateToAngle(Rotation2d.fromRadians(0));
+		frontRight.rotateToAngle(Rotation2d.fromRadians(0));
+		backLeft.rotateToAngle(Rotation2d.fromRadians(0));
+		backRight.rotateToAngle(Rotation2d.fromRadians(0));
+	}
+
 	@Override
 	public void updateInputs(SwerveChassisInputsAutoLogged inputs) {
 		inputs.isVisionEnabled = doVision;
