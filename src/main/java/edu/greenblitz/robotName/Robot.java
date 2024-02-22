c3310ee310b4a1ae803d2a17c50b3004f3d5fd00
hillelv <hillel.vider@gmail.com>
2024-02-22 16:25:00 +0200

    hillel - now there are two ways to shoot

--- src/main/java/edu/greenblitz/robotName/Robot.java
+++ src/main/java/edu/greenblitz/robotName/Robot.java
@@ -88,7 +88,8 @@ public class Robot extends LoggedRobot {
 	}
 
 	private void initializeAutonomousBuilder() {
-		NamedCommands.registerCommand("shoot", new ShootFromInFunnel());
+		NamedCommands.registerCommand("shoot", new GoToShootingStateAndShoot(ShootingPositionConstants.OPTIMAL_SHOOTING_ZONE));
+		NamedCommands.registerCommand("close shoot", new ShootFromInFunnel());
 		NamedCommands.registerCommand("grip", new NoteToShooter().raceWith(new WaitCommand(1)));
 		AutoBuilder.configureHolonomic(
 				SwerveChassis.getInstance()::getRobotPose2d,
