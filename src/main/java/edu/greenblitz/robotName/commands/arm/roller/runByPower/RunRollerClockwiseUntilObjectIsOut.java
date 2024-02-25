d73750c311a55833d5d550da88f99f33b84f9ac4
itamaroryan <itamar.oryan@gmail.com>
2024-02-25 19:19:48 +0200

    Yoni - changed in all of the code the isObjectIn

--- src/main/java/edu/greenblitz/robotName/commands/arm/roller/runByPower/RunRollerClockwiseUntilObjectIsOut.java
+++ src/main/java/edu/greenblitz/robotName/commands/arm/roller/runByPower/RunRollerClockwiseUntilObjectIsOut.java
@@ -1,9 +1,18 @@
 package edu.greenblitz.robotName.commands.arm.roller.runByPower;
 
 import edu.greenblitz.robotName.commands.arm.roller.RollerCommand;
+import edu.greenblitz.robotName.subsystems.arm.roller.RollerConstants;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public class RunRollerClockwiseUntilObjectIsOut extends RollerCommand {
-
+    
+    private Rotation2d targetPosition;
+    
+    @Override
+    public void initialize() {
+        targetPosition = roller.getAngle().plus(RollerConstants.ROTATIONS_TILL_OBJECT_EXITED);
+    }
+    
     @Override
     public void execute() {
         roller.rollClockwise();
@@ -11,6 +20,12 @@ public class RunRollerClockwiseUntilObjectIsOut extends RollerCommand {
 
     @Override
     public boolean isFinished() {
-        return !roller.isObjectIn();
+        return roller.isAtAngle(targetPosition);
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        roller.setObjectInside(false);
     }
 }
