91aee9d1282b7154a5a1007e85ab099f4d046d65
itamaroryan <itamar.oryan@gmail.com>
2024-02-25 23:49:21 +0200

    Yoni í»the goat - made stuff roller lets gooo

--- src/main/java/edu/greenblitz/robotName/subsystems/arm/roller/bagRoller/BagRoller.java
+++ src/main/java/edu/greenblitz/robotName/subsystems/arm/roller/bagRoller/BagRoller.java
@@ -6,6 +6,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.greenblitz.robotName.subsystems.Battery;
 import edu.greenblitz.robotName.subsystems.arm.roller.IRoller;
 import edu.greenblitz.robotName.subsystems.arm.roller.RollerInputsAutoLogged;
+import edu.greenblitz.robotName.subsystems.arm.roller.neoRoller.NeoRollerConstants;
 import edu.greenblitz.robotName.utils.Conversions;
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -13,8 +14,12 @@ public class BagRoller implements IRoller {
 	
 	private TalonSRX motor;
 	
+	private RollerInputsAutoLogged lastInputs;
+	
 	public BagRoller() {
 		motor = new TalonSRX(BagRollerConstants.MOTOR_ID);
+		
+		lastInputs = new RollerInputsAutoLogged();
 	}
 	
 	@Override
@@ -37,11 +42,18 @@ public class BagRoller implements IRoller {
 		motor.set(TalonSRXControlMode.Position, position.getRotations());
 	}
 	
+	public boolean isObjectInByCurrent(double current){
+		return Math.abs(current - lastInputs.outputCurrent) >= NeoRollerConstants.NOTE_IN_CURRENT
+				|| current >= NeoRollerConstants.NOTE_IN_CURRENT;
+	}
 	
 	@Override
 	public void updateInputs(RollerInputsAutoLogged rollerInputs) {
 		rollerInputs.appliedOutput = motor.getMotorOutputVoltage();
 		rollerInputs.outputCurrent = motor.getOutputCurrent();
 		rollerInputs.position = Rotation2d.fromRotations(motor.getSelectedSensorPosition());
+		rollerInputs.isObjectIn = isObjectInByCurrent(rollerInputs.outputCurrent);
+		
+		lastInputs = rollerInputs;
 	}
 }
