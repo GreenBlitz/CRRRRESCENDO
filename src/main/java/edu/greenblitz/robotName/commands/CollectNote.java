a7ab6131cfcb652ef7157e8aa0489b3ba0203432
YoniKiriaty <YoniKiriaty@gmail.com>
2024-02-26 19:52:40 +0200

    Ran - didnt want to remove your work

--- src/main/java/edu/greenblitz/robotName/commands/CollectNote.java
+++ src/main/java/edu/greenblitz/robotName/commands/CollectNote.java
@@ -3,6 +3,7 @@ package edu.greenblitz.robotName.commands;
 import edu.greenblitz.robotName.commands.arm.MoveArmToIntakePosition;
 import edu.greenblitz.robotName.commands.intake.NoteFromIntakeToShooter;
 import edu.greenblitz.robotName.commands.intake.NoteToIntake;
+import edu.greenblitz.robotName.commands.intake.NoteToShooter;
 import edu.greenblitz.robotName.commands.shooter.MoveShooterToAngle;
 import edu.greenblitz.robotName.subsystems.shooter.pivot.Pivot;
 import edu.greenblitz.robotName.subsystems.shooter.pivot.PivotConstants;
@@ -12,9 +13,9 @@ public class CollectNote extends ConditionalCommand {
 
     public CollectNote() {
         super(
-                new NoteToIntake()
-                        .andThen(new MoveArmToIntakePosition())
-                        .alongWith(new NoteFromIntakeToShooter()),
+                new NoteToShooter()
+                        .alongWith(new MoveArmToIntakePosition())
+                        .alongWith(new ),
                 new NoteToIntake()
                         .alongWith(new MoveShooterToAngle(PivotConstants.PresetPositions.PICK_UP.ANGLE))
                         .andThen(new MoveArmToIntakePosition())
