4e8e71060216de873f58fcb15167a453af3d82c0
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-02-27 14:58:18 +0200

    danna - fixed collect note command

--- src/main/java/edu/greenblitz/robotName/commands/CollectNote.java
+++ src/main/java/edu/greenblitz/robotName/commands/CollectNote.java
@@ -1,6 +1,7 @@
 package edu.greenblitz.robotName.commands;
 
 import edu.greenblitz.robotName.commands.arm.MoveArmToIntakePosition;
+import edu.greenblitz.robotName.commands.arm.roller.runByPower.RollClockwise;
 import edu.greenblitz.robotName.commands.intake.NoteFromIntakeToShooter;
 import edu.greenblitz.robotName.commands.intake.NoteToIntake;
 import edu.greenblitz.robotName.commands.intake.NoteToShooter;
@@ -15,10 +16,11 @@ public class CollectNote extends ConditionalCommand {
         super(
                 new NoteToShooter()
                         .alongWith(new MoveArmToIntakePosition())
-                        .alongWith(new ),
+                        .andThen(new RollClockwise()),
                 new NoteToIntake()
                         .alongWith(new MoveShooterToAngle(PivotConstants.PresetPositions.PICK_UP.ANGLE))
                         .andThen(new MoveArmToIntakePosition())
+                        .alongWith(new RollClockwise())
                         .alongWith(new NoteFromIntakeToShooter()),
                 () -> Pivot.getInstance().isAtAngle(PivotConstants.PresetPositions.PICK_UP.ANGLE)
         );
