fd1f0e12d3fc88558bf50a5cf7a85d698f68dd08
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-03-04 12:59:44 +0200

    Amir - finished the vision and added the move pivot to angle to e default command

--- src/main/java/edu/greenblitz/robotName/commands/shooter/pivot/PivotDefaultCommand.java
+++ src/main/java/edu/greenblitz/robotName/commands/shooter/pivot/PivotDefaultCommand.java
@@ -1,21 +1,15 @@
 package edu.greenblitz.robotName.commands.shooter.pivot;
 
 
-import edu.greenblitz.robotName.Robot;
+import edu.greenblitz.robotName.shootingStateService.ShootingPositionConstants;
+import edu.greenblitz.robotName.shootingStateService.ShootingStateCalculations;
 
 public class PivotDefaultCommand extends PivotCommand {
 
-	@Override
-	public void initialize() {
-		pivot.setCurrentAngle();
-		pivot.standInPlace();
-	}
 
 	@Override
 	public void execute() {
-		if (Robot.isSimulation()) {
-			pivot.standInPlace();
-		}
+		pivot.moveToAngle(() -> ShootingStateCalculations.getTargetShooterAngle(ShootingPositionConstants.LEGAL_SHOOTING_ZONE));
 	}
 
 }
