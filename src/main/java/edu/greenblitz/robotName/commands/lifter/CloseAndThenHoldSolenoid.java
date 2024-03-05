f1922f8296fbae58aa7aa6a1ab5034dcec2a40cb
RJ <ranelj2008@gmail.com>
2024-03-05 20:38:21 +0200

    Ran - added the new command to the sequence

--- src/main/java/edu/greenblitz/robotName/commands/lifter/CloseAndThenHoldSolenoid.java
+++ src/main/java/edu/greenblitz/robotName/commands/lifter/CloseAndThenHoldSolenoid.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class CloseAndThenHoldSolenoid extends SequentialCommandGroup {
 	public CloseAndThenHoldSolenoid(){
 		super(
+				new PushSolenoidWheel(),
 				new SolenoidClose(),
 				new SolenoidHold()
 		);
