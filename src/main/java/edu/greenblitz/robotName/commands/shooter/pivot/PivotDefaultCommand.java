e62dadeed0183917bfd06c8b30530abfc3bcec18
greenblitz4590 <94316058+greenblitz4590@users.noreply.github.com>
2024-03-12 18:24:15 +0200

    Hillel & Yoni - added default command

--- src/main/java/edu/greenblitz/robotName/commands/shooter/pivot/PivotDefaultCommand.java
+++ src/main/java/edu/greenblitz/robotName/commands/shooter/pivot/PivotDefaultCommand.java
@@ -1,20 +1,14 @@
 package edu.greenblitz.robotName.commands.shooter.pivot;
 
 
-import edu.greenblitz.robotName.Robot;
 import edu.greenblitz.robotName.shootingStateService.ShootingPositionConstants;
 import edu.greenblitz.robotName.shootingStateService.ShootingStateCalculations;
-import edu.wpi.first.math.geometry.Rotation2d;
 
-public class PivotDefaultCommand extends PivotCommand {
+public class PivotDefaultCommand extends MovePivotToAngle {
 
-	public void initialize(){
-		super.initialize();
-		pivot.setCurrentAngle();
-	}
-	
-	@Override
-	public void execute() {
-		pivot.standInPlace();
+	public PivotDefaultCommand() {
+		super(() -> ShootingStateCalculations.getTargetShooterAngle(
+				ShootingPositionConstants.LEGAL_SHOOTING_ZONE
+		));
 	}
 }
