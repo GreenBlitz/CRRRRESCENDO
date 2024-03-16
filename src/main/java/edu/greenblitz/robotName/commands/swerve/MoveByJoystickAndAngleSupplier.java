7f1e735604eda7a1e49eadf00db63f13ded80252
YHerm <0549405422.yh@gmail.com>
2024-03-16 21:20:33 +0200

    yoav - added

---
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/MoveByJoystickAndAngleSupplier.java
@@ -0,0 +1,89 @@
+package edu.greenblitz.robotName.commands.swerve;
+
+import edu.greenblitz.robotName.OI;
+import edu.greenblitz.robotName.subsystems.swerve.chassis.ChassisConstants;
+import edu.greenblitz.robotName.utils.hid.SmartJoystick;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import java.util.function.DoubleSupplier;
+
+import static edu.greenblitz.robotName.subsystems.swerve.chassis.ChassisConstants.MAX_ANGULAR_SPEED;
+import static edu.greenblitz.robotName.subsystems.swerve.chassis.ChassisConstants.ROTATION_PID_CONTROLLER;
+
+public class MoveByJoystickAndAngleSupplier extends SwerveCommand {
+
+    private double angularSpeedFactor;
+
+    private double linearSpeedFactor;
+
+    private DoubleSupplier angularVelocitySupplier;
+
+    private MoveByJoysticks.DriveMode driveMode;
+
+    private Rotation2d targetAngle;
+
+    private double factor = 1;
+    private double maxAngularVelocity = 6;
+
+    public MoveByJoystickAndAngleSupplier(MoveByJoysticks.DriveMode driveMode, DoubleSupplier angularVelocitySupplier, Rotation2d angle) {
+        this.driveMode = driveMode;
+        this.angularVelocitySupplier = angularVelocitySupplier;
+        this.targetAngle = angle;
+    }
+
+    public MoveByJoystickAndAngleSupplier(MoveByJoysticks.DriveMode driveMode, Rotation2d angle) {
+        this(
+                driveMode,
+                () -> OI.getInstance().getMainJoystick().getAxisValue(SmartJoystick.Axis.RIGHT_X),
+                angle
+        );
+    }
+
+    @Override
+    public void initialize() {
+        ROTATION_PID_CONTROLLER.setSetpoint(targetAngle.getRadians());
+        switch (driveMode) {
+            case SLOW -> {
+                linearSpeedFactor = ChassisConstants.DRIVER_LINEAR_SPEED_FACTOR_SLOW;
+                angularSpeedFactor = ChassisConstants.DRIVER_ANGULAR_SPEED_FACTOR_SLOW;
+            }
+            case NORMAL -> {
+                linearSpeedFactor = ChassisConstants.DRIVER_LINEAR_SPEED_FACTOR;
+                angularSpeedFactor = ChassisConstants.DRIVER_ANGULAR_SPEED_FACTOR;
+            }
+        }
+    }
+
+    @Override
+    public void execute() {
+        double leftwardSpeed = OI.getInstance().getMainJoystick().getAxisValue(SmartJoystick.Axis.LEFT_X) * linearSpeedFactor;
+
+        double forwardSpeed = -OI.getInstance().getMainJoystick().getAxisValue(SmartJoystick.Axis.LEFT_Y) * linearSpeedFactor;
+
+        double angularSpeed = -angularVelocitySupplier.getAsDouble() * angularSpeedFactor;
+
+        if (forwardSpeed == 0 && leftwardSpeed == 0 && angularSpeed == 0) {
+            swerveChassis.stop();
+            return;
+        }
+
+
+        double pidVelocity =  ChassisConstants.ROTATION_PID_CONTROLLER.calculate(swerveChassis.getChassisAngle().getRadians());
+        double axesSpeed = Math.abs(leftwardSpeed) + Math.abs(forwardSpeed);
+        double velocity = pidVelocity * factor/(axesSpeed + factor);
+        double angularVelocityWithJoystick = velocity + angularSpeed;
+        double checkedVelocity = Math.min(angularVelocityWithJoystick, MAX_ANGULAR_SPEED);
+
+        swerveChassis.moveByChassisSpeeds(
+                forwardSpeed,
+                leftwardSpeed,
+                checkedVelocity,
+                swerveChassis.getChassisAngle()
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+}
---
+++ src/main/java/edu/greenblitz/robotName/commands/swerve/RotateToSpeakerWhileMove.java
@@ -0,0 +1,14 @@
+package edu.greenblitz.robotName.commands.swerve;
+
+import edu.greenblitz.robotName.shootingStateService.ShootingStateCalculations;
+import edu.greenblitz.robotName.subsystems.swerve.chassis.ChassisConstants;
+
+public class RotateToSpeakerWhileMove extends MoveByJoystickAndAngleSupplier{
+
+    public RotateToSpeakerWhileMove(){
+        super(
+                ChassisConstants.DRIVE_MODE,
+                ShootingStateCalculations.getTargetRobotAngle()
+        );
+    }
+}
